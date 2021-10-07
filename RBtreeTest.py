5314a607dba27459cf9ca895c87144aabfac75cb
GarryG00 <GarryG00@users.noreply.github.com>
2021-10-07 16:58:31 -0500

    prop4 check

--- RBtreeTest.py
+++ RBtreeTest.py
@@ -2,6 +2,30 @@ import RBtree as rbt
 import numpy as np
 import random
 
+def checkChild(node):
+    return (node.left.c == False and node.right.c == False)
+        
+def prop4helper(node):
+    if node.key == "NIL":
+        return True
+    if node.c == True:
+        rst = checkChild(node)
+        if not rst:
+            return False
+        
+    x = prop4helper(node.left)
+    y = prop4helper(node.right)
+    return (x and y)
+
+def prop4(T):
+    rst = prop4helper(T.root)
+    if rst:
+        print("Property 4 holds")
+    else: 
+        print("Property 4 does not hold")
+
+    return
+
 #insertion test block
 #insertion test completed
 insert = 0
@@ -24,7 +48,7 @@ if insert:
 #case needed: delete same node twice
 #case needed: delete from empty tree
 #case needed: delete non-existent node
-delete = 1
+delete = 0
 if delete: 
     print("delete test block start:")
     testblock = [10, 80, 92, 42, 28, 26, 73, 45, 41, 97]
@@ -93,3 +117,22 @@ if search:
     T.delete(nodes["Node0"])
     print(T.searchKey(10))
     print(T.searchKey(45))
+
+#prop4 test block
+#prop4 test incomplete
+
+prop4test = 1
+if prop4test:
+    print("prop4 test block start:")
+    testblock = [10, 80, 92, 42, 28, 26, 73, 45, 41, 97]
+    T = rbt.RedBlackTree()
+    numNode = len(testblock)
+    nodes = {}
+
+    for i in range(numNode):
+        nodes["Node"+str(i)] = rbt.RedBlackNode(testblock[i])
+        T.insert(nodes["Node"+str(i)])
+        #print(i+1, " elements added, value =", testblock[i])
+
+    print("Tree init done")
+    prop4(T)
